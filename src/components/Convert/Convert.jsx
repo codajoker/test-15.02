3df724eadfefbbc7908d7bbaa7b801e8f364d405
codajoker <codajoker91@gmail.com>
2022-02-15 21:50:40 +0200

    Change

--- src/components/Convert/Convert.jsx
+++ src/components/Convert/Convert.jsx
@@ -1,46 +1,71 @@
-import { useState } from 'react';
-import { Wrapper, ResultBlock } from './ConvertStyled';
+import { useEffect, useState } from 'react';
+import {
+  Wrapper,
+  ResultBlock,
+  Input,
+  Select,
+  Container,
+} from './ConvertStyled';
+import { FaEquals } from 'react-icons/fa';
 
 export function Convert({ arrayMoney }) {
   const [firstInput, setFirstInput] = useState(0);
   const [firstSelect, setFirstSelect] = useState('USD');
+  const [resultInput, setResultInput] = useState(0);
   const [resultSelect, setResultSelect] = useState('EUR');
+  useEffect(() => {
+    console.log(arrayMoney);
+    setResultInput(
+      (Number(arrayMoney.find(option => option.ccy === firstSelect).buy) *
+        firstInput) /
+        Number(arrayMoney.find(option => option.ccy === resultSelect).buy)
+    );
+  }, [firstInput, resultSelect]);
+  useEffect(() => {
+    setFirstInput(
+      (Number(arrayMoney.find(option => option.ccy === resultSelect).buy) *
+        resultInput) /
+        Number(arrayMoney.find(option => option.ccy === firstSelect).buy)
+    );
+  }, [resultInput, firstSelect]);
   return (
-    <Wrapper>
-      <div>
+    <Container>
+      <Wrapper>
         {' '}
-        <input
+        <Input
           onChange={e => setFirstInput(e.target.value)}
           type="number"
-          id="val"
           placeholder="Введите сумму..."
+          value={firstInput}
         />
-        <select
+        <Select
           defaultValue={firstSelect}
           onChange={e => setFirstSelect(e.target.value)}
-          id="cur1"
         >
           {arrayMoney.map(({ ccy }) => (
             <option key={ccy}> {ccy}</option>
           ))}
-        </select>
-      </div>
-      <ResultBlock>
-        <div>
-          ={' '}
-          {(Number(arrayMoney.find(option => option.ccy === firstSelect).buy) *
-            firstInput) /
-            Number(arrayMoney.find(option => option.ccy === resultSelect).sale)}
-        </div>
-        <select
+        </Select>
+      </Wrapper>
+      <FaEquals></FaEquals>
+      <Wrapper>
+        {' '}
+        <Input
+          type="number"
+          id="val"
+          placeholder="Введите сумму..."
+          onChange={e => setResultInput(e.target.value)}
+          value={resultInput}
+        ></Input>
+        <Select
           defaultValue={resultSelect}
           onChange={e => setResultSelect(e.target.value)}
         >
           {arrayMoney.map(({ ccy }) => (
             <option key={ccy}> {ccy}</option>
           ))}
-        </select>
-      </ResultBlock>
-    </Wrapper>
+        </Select>
+      </Wrapper>
+    </Container>
   );
 }
--- src/components/Convert/ConvertStyled.jsx
+++ src/components/Convert/ConvertStyled.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
-
-export const Wrapper = styled.div`
+export const Container = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 50px;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  padding: 50px;
 `;
 export const FirstBlock = styled.div`
   display: flex;
@@ -11,8 +15,32 @@ export const FirstBlock = styled.div`
 export const ResultBlock = styled.div`
   display: flex;
 `;
-// export const Wrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   margin-top: 50px;
-// `;
+export const Input = styled.input`
+  height: calc(2.25rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #bdbdbd;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`;
+export const Select = styled.select`
+  margin-left: 20px;
+  height: calc(2.25rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #bdbdbd;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`;
